23ca32c31c341ada543c3806f01654354532d054
Home <Baza4905>
2021-11-16 14:09:02 +0300

    Расширенное задание. Lesson5

--- src/test/java/TestIntegration.java
+++ src/test/java/TestIntegration.java
@@ -17,7 +17,7 @@ class TestIntegration {
 
     @Test
     public void eqalsSearchTest() {
-        Main.main(new String[]{"-f", "test-files.xml", "-s", "file-22222222.txt"});
+        Main.main(new String[]{"-f", "src/test/resources/test-files.xml", "-s", "file-22222222.txt"});
         String result = "/dir-88971375/dir-219753795/file-22222222.txt\r\n";
         assertEquals(result, outContent.toString());
     }
@@ -25,7 +25,7 @@ class TestIntegration {
     @Test
     public void maskSearchTest() {
 
-        Main.main(new String[]{"-f", "test-files.xml", "-s", "*.txt"});
+        Main.main(new String[]{"-f", "src/test/resources/test-files.xml", "-s", "*.txt"});
         String result = "/dir-88971375/file-1272239243.txt\r\n"
                 + "dir-88971375/dir-219753795/file-22222222.txt\r\n"
                 + "dir-88971375/dir-219753795/file.txt\r\n";
@@ -35,7 +35,7 @@ class TestIntegration {
     @Test
     public void regularSearchTest() {
 
-        Main.main(new String[]{"-f", "test-files.xml", "-S", ".*?[a-z]{4}-\\d+.[a-z]+"});
+        Main.main(new String[]{"-f", "src/test/resources/test-files.xml", "-S", ".*?[a-z]{4}-\\d+.[a-z]+"});
         String result = "/file-77194797.xmml\r\n"
                 + "dir-88971375/file-9738721998.java\r\n"
                 + "dir-88971375/file-1272239243.txt\r\n"
@@ -49,7 +49,7 @@ class TestIntegration {
     @Test
     public void fullSearchTest() {
 
-        Main.main(new String[]{"-f", "test-files.xml"});
+        Main.main(new String[]{"-f", "src/test/resources/test-files.xml"});
         String result = "/file-77194797.xmml\r\n"
                 + "dir-88971375/file-9738721998.java\r\n"
                 + "dir-88971375/file-1272239243.txt\r\n"
